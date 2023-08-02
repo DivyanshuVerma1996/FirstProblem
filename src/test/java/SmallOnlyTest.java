1aafbf11d4ecb9187e42bf2534d1dc966e43c2a8
Divyanshu Verma <in45704558@inmlhxk2k69l.global.tesco.org>
2023-08-02 14:58:58 +0530

    changed SmallOnlyTest file

--- src/test/java/SmallOnlyTest.java
+++ src/test/java/SmallOnlyTest.java
@@ -23,5 +23,10 @@ public class SmallOnlyTest{
         small.find(new int[]{4,3,2});
         Assert.assertEquals(2,small.getSmaller());
     }
-
+    @Test
+    public void onlyOneElement()
+    {
+        small.find(new int[]{3});
+        Assert.assertEquals(3,small.getSmaller());
+    }
 }
